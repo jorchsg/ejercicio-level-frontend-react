f7afb5b9019a0a6cd87199bbb2475a0bd923f8b3
jorchsg <sandoval.jorge.407@gmail.com>
2020-09-21 23:55:40 -0700

    Iniciando componente de formulario y css main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <h1>Revisa Tus Resultados</h1>
+
+    <Fragment>
+
+      <h1>Revisa Tus Resultados</h1>
+
+      <div className="container">
+
+        <div className="row">
+
+          <div className="one-half column">
+            <Formulario
+
+            />
+          </div>
+
+          <div className="one half-column">
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </Fragment>
+
   );
 }
 
