8c5b9f70cb8594bba89ac533aa954a6e2fbe9c6a
jorchsg <sandoval.jorge.407@gmail.com>
2020-09-22 00:19:04 -0700

    Generando los unputs

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
     return (
-        <h1>Formulario</h1>
+
+        <Fragment>
+
+            <h1>Datos Semanales</h1>
+            <form>
+                <label>Nombre: </label>
+                <input
+                    type="text"
+                    name="nombre"
+                    className="u-full-width"
+                    placeholder="Nombre y Apellido"
+                />
+                <label>Correo: </label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    name="correo"
+                    placeholder="ejemplo@ejemplo.com"
+                />
+                <label>KM Recorridos: </label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    name="recorrido"
+                    placeholder="0"
+                    min="0"
+                    max="200"
+                />
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Confirmar Datos</button>
+
+            </form>
+
+        </Fragment>
     );
 }
 
